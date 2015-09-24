b65f1fcdcbe6ba010d94b97fc2eb105615cf16bc
Sangchul Lee <sc11.lee@samsung.com>
2015-09-24 15:04:03 +0900

    stream-manager: Support a role for external devices device-manager: Add sink/source state changed hook for updating state of external devices policy: Add routing logic for external devices
    [Version] 5.0-42
    [Profile] Common
    [Issue Type] Enhance feature
    
    A role that has the prefix of "ext-" is only for external audio devices that have their own audio codec.(eg.,usb,bluetooth)
    This is an internal rule for our implementation with stream-map.json file.
    With this policy, we can control external devices independently of internal devices.
    
    Change-Id: I6b48d7558a3978b2538de3dbbc8d27209a755ec0
    Signed-off-by: Sangchul Lee <sc11.lee@samsung.com>

--- src/stream-manager.h
+++ src/stream-manager.h
@@ -2,6 +2,16 @@
 #define foostreammanagerfoo
 #include <pulsecore/core.h>
 
+/* NOTE: A role that has this prefix is only for external audio devices.
+ * This is an internal rule for our implementation with stream-map.json file.
+ * Before long, we need to improve it for more generalization. */
+#define PREFIX_ROLE_FOR_EXTERNAL_DEV "ext-"
+#define IS_ROLE_FOR_EXTERNAL_DEV(role) \
+      (!strncmp(PREFIX_ROLE_FOR_EXTERNAL_DEV, role, strlen(PREFIX_ROLE_FOR_EXTERNAL_DEV)))
+#define PREFIX_ROLE_FOR_FILTER "filter"
+#define IS_ROLE_FOR_FILTER(role) \
+      (!strncmp(PREFIX_ROLE_FOR_FILTER, role, strlen(PREFIX_ROLE_FOR_FILTER)))
+
 typedef struct _stream_manager pa_stream_manager;
 
 typedef enum _stream_type {
@@ -16,6 +26,7 @@ typedef enum stream_route_type {
 } stream_route_type_t;
 
 typedef struct _hook_call_data_for_select {
+    void *stream;
     const char *stream_role;
     stream_type_t stream_type;
     stream_route_type_t route_type;
@@ -24,6 +35,7 @@ typedef struct _hook_call_data_for_select {
     pa_sample_spec sample_spec;
     pa_idxset *idx_avail_devices;
     pa_idxset *idx_manual_devices;
+    pa_bool_t origins_from_new_data;
 } pa_stream_manager_hook_data_for_select;
 
 typedef struct _hook_call_data_for_route {

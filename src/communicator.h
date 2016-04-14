cd886f151653d56533bb732173acf688700be7f1
Sangchul Lee <sc11.lee@samsung.com>
2016-04-14 17:03:23 +0900

    policy: Revise codes
    stream-mananager: Call pa_hal_manager_update_route_option() directly rather than use communicator hook.
    communicator: Add PA_COMMUNICATOR_HOOK_UPDATE_INFORMATION, remove PA_COMMUNICATOR_HOOK_UPDATE_ROUTE_OPTION and update description.
    policy: Use PA_COMMUNICATOR_HOOK_UPDATE_INFORMATION to get notified arguments of module-loopback from HAL.
    
    [Version] 5.0.44
    [Profile] Common
    [Issue Type] Refactorying
    
    Change-Id: I0a5a4631dac1fffd05cef935c62bc4ff5ced367c

--- src/communicator.h
+++ src/communicator.h
@@ -3,13 +3,12 @@
 #include <pulsecore/core.h>
 
 typedef enum pa_communicator_hook {
-    PA_COMMUNICATOR_HOOK_SELECT_INIT_SINK_OR_SOURCE,
-    PA_COMMUNICATOR_HOOK_CHANGE_ROUTE,
-    PA_COMMUNICATOR_HOOK_UPDATE_ROUTE_OPTION,
-    PA_COMMUNICATOR_HOOK_DEVICE_CONNECTION_CHANGED,
-    PA_COMMUNICATOR_HOOK_DEVICE_INFORMATION_CHANGED,
-    /* This hook is fired when a event is handled by all subscribers */
-    PA_COMMUNICATOR_HOOK_EVENT_FULLY_HANDLED,
+    PA_COMMUNICATOR_HOOK_SELECT_INIT_SINK_OR_SOURCE, /* It is fired when a stream is created and needs to be set to sink or source */
+    PA_COMMUNICATOR_HOOK_CHANGE_ROUTE,               /* It is fired when routing using internal codec should be processed */
+    PA_COMMUNICATOR_HOOK_DEVICE_CONNECTION_CHANGED,  /* It is fired when a device is connected or disconnected */
+    PA_COMMUNICATOR_HOOK_DEVICE_INFORMATION_CHANGED, /* It is fired when a device's property is changed */
+    PA_COMMUNICATOR_HOOK_EVENT_FULLY_HANDLED,        /* It is fired when a event is handled by all subscribers */
+    PA_COMMUNICATOR_HOOK_UPDATE_INFORMATION,         /* It is fired when information should be updated */
     PA_COMMUNICATOR_HOOK_MAX
 } pa_communicator_hook_t;
 

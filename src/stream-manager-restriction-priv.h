a4d5f0f15ecd079f25e4f63cbf3ee9eb8cb2bba0
Sangchul Lee <sc11.lee@samsung.com>
2016-04-27 18:45:02 +0900

    stream-manager: Add support API for stream restriction
    dbus method is added.
     - name: UpdateRestriction
     - args: name(string[in]), value(uint32[in]), ret_msg(string[out])
    
    We support "block_recording_media" as the name for now.
    It can be called when recording streams of media role should be blocked.
    
    [Version] 5.0.49
    [Profile] Common
    [Issue Type] New feature
    
    Signed-off-by: Sangchul Lee <sc11.lee@samsung.com>
    Change-Id: I4f7ef2f3ab7a824456bff57a44fe99a1e6ad284b

---
+++ src/stream-manager-restriction-priv.h
@@ -0,0 +1,12 @@
+#ifndef foostreammanagerrestrictionprivfoo
+#define foostreammanagerrestrictionprivfoo
+
+#include "stream-manager.h"
+
+/* dbus method args */
+#define STREAM_MANAGER_METHOD_ARGS_BLOCK_RECORDING_MEDIA     "block_recording_media"
+
+int32_t handle_restrictions(pa_stream_manager *m, const char *name, uint32_t value);
+bool check_restrictions(pa_stream_manager *m, void *stream, stream_type_t stream_type);
+
+#endif
